114abb52669ff8bd63efc85c24a53063a6dd716a
Abdul Mussawir <111042314+AbdulMussawir1994@users.noreply.github.com>
2025-10-26 12:24:41 +0500

    Updated Home

--- V9MvcCoreProject/Controllers/HomeController.cs
+++ V9MvcCoreProject/Controllers/HomeController.cs
@@ -19,11 +19,22 @@ public class HomeController : Controller
         return View();
     }
 
+    [HttpGet]
+    // [AllowAnonymous]
+    [CheckUserSession]
+    public JsonResult GetSelectList()
+    {
+        List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 4, 6, 3, 6, 7 };
+        var result = numbers.Distinct().Select(x => new { Id = x, Name = "Number " + x }).ToList();
+        return Json(result);
+    }
+
     [ServiceFilter(typeof(CheckUserPermission))]
     public IActionResult Privacy()
     {
         return View();
     }
+
 
     [ServiceFilter(typeof(CheckUserPermission))]
     public IActionResult Test()
---
+++ V9MvcCoreProject/wwwroot/ViewsScripts/Index.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    GetListData();
+
+});
+
+
+function GetListData() {
+    $.ajax({
+        type: "GET",
+        url: "/Home/GetSelectList",
+        dataType: "json",
+        success: function (response) {
+            console.log("List data loaded:", response);
+
+            const $dropDown = $("#listId");
+            $dropDown.empty().append('<option value ="">Select List</option>');
+
+            if (response && response.length > 0) {
+                $.each(response, function (i, item) {
+                    $dropDown.append($("<option></option>").val(item.Id).text(item.Name)
+                    );
+                });
+            }
+            else {
+                $dropDown.append('<option value="">No Lists Available</option>');
+            }
+        },
+        error: function (xhr) {
+            console.error("Error loading list data:", xhr.responseText);
+            alert(xhr);
+        }
+    });
+}
